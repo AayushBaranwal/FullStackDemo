7150987dad64579f96e3b306c50a3028dd5ec444
Ayush <baranwal11ayush@gmail.com>
2025-12-10 15:40:49 +0530

    Remove start.sh – using Nixpacks config instead

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# go into backend folder
-cd backend
-
-# build the Spring Boot app (skip tests to be faster)
-./mvnw -DskipTests package
-
-# run the jar (change name if your jar is different)
-java -jar target/demo-0.0.1-SNAPSHOT.jar
